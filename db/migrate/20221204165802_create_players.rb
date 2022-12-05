c3d343fb38cbc49cc10802883bbbb13f086601e3
Chett Tiller <chett.tiller@gmail.com>
2022-12-05 16:13:11 -0500

    made last_name a string instead of an integer

--- db/migrate/20221204165802_create_players.rb
+++ db/migrate/20221204165802_create_players.rb
@@ -2,7 +2,7 @@ class CreatePlayers < ActiveRecord::Migration[6.1]
   def change
     create_table :players do |t|
       t.string :first_name
-      t.integer :last_name
+      t.string :last_name
     end
   end
 end
